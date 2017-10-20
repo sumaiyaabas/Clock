1fa6a30ac12487175902e6887e6a7a5c3bdf253f
sumaiyaabas <32968320+sumaiyaabas@users.noreply.github.com>
2017-10-20 16:09:14 -0500

    Add files via upload

---
+++ clockClass.java
@@ -0,0 +1,45 @@
+// **********************************************************************
+// Programmer:	Rob Sveinson
+// Class:		CS20S
+//
+// Assignment:	aX qy
+//
+// Description:	a brief description of the program goes here
+//
+//
+//	Input:		describe any input from keyboard or file
+//
+//  Output:		describe the result of your program
+// ***********************************************************************
+
+
+public class clockClass/*CHANGE THIS TO THE NAME OF THE FILE*/
+{  
+// begin class
+	public static void main(String args[])
+	{  // begin main
+   
+ // *********************** Declaration of Variables ***************************
+       
+	
+	
+
+ //*******************************Output Banner*************************
+
+            System.out.println("**********************************");
+            System.out.println("Name: Sumaiya Abas");
+            System.out.println("Class: CS30S");
+            System.out.println("Assignment: clockClass");
+            System.out.println("**********************************");
+
+    //************************ Constructor*****************************
+       //int currentSecond;
+    
+    
+
+    // ********************** closing message *************
+	
+        System.out.println("end of processing");
+	
+	}  // end main	
+}  // end class
